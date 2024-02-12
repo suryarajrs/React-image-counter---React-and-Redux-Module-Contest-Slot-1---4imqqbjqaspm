fe7f41a8b5edb1a95768fe554b8a5fb8af7d1d67
suryarajrs <suryars@outlook.in>
2024-02-13 00:56:47 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+
+  const [height,setHeight] = useState(300);
+  const [width , setWidth] = useState(300)
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img onClick={()=>{ setHeight(height+2) ; setWidth(width+2)}} src={star}  height={height} width={width} />
     </div>
   )
 }
